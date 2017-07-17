6fff8aa2e07c73d96fbe6c6e9aace5bae000d3b5
taingesrun <sruntainge2015@gmail.com>
2017-07-17 22:15:04 +0700

    First commit

---
+++ article-management-multi-modules/src/main/java/com/article/ArticleManagementMultiMudulesApplication.java
@@ -0,0 +1,12 @@
+package com.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleManagementMultiMudulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleManagementMultiMudulesApplication.class, args);
+	}
+}
